c67a04b11c74dea87946a334f1bd82fd78592403
Cleidiane Gonçalves <cleidianeagoncalves@gmail.com>
2021-07-05 22:49:15 -0300

    Alteração de annotation da Entity para correção de problema no Heroku

--- src/Entity/Trade.php
+++ src/Entity/Trade.php
@@ -6,7 +6,7 @@ use App\Repository\TradeRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass=TradeRepository::class)
+ * @ORM\Entity(repositoryClass="App\Repository\TradeRepository")
  */
 class Trade
 {
